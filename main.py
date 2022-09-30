9d03467f8d9a260b0bf3e45fe3a449417c50e415
Nigel Harsch <nigelharsch@gmail.com>
2022-09-30 19:16:33 -0400

    loop through directory

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for filename in os.listdir(CSV_DIR):
     path = os.path.join(CSV_DIR, filename)
     with open(path) as file:
         reader = csv.reader(file)
-        table_name = filename.strip(".csv")
+        table_name = filename.replace(".csv", "")
         headers = next(reader)
         erd_str = "{table_name} {{ {columns} }}".format(table_name=table_name.upper(),
                                                         columns="\n".join("string {}".format(c) for c in headers))
